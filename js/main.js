7d008a2fd1b8609ac09115079441119d50b1237b
Maz47 <maz47.dev@gmail.com>
2020-11-12 15:50:57 +0100

    dark mode added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+const theme = localStorage.getItem('theme');
+if (theme === "dark") {
+  document.documentElement.setAttribute('data-theme', 'dark');
+}
+
+const userPrefers = getComputedStyle(document.documentElement).getPropertyValue('content');	
+
+if (theme === "dark") {
+	//document.getElementById("theme-toggle").innerHTML = "Light Mode";
+} else if (theme === "light") {
+	//document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+} else if  (userPrefers === "dark") {
+	document.documentElement.setAttribute('data-theme', 'dark');
+	window.localStorage.setItem('theme', 'dark');
+	//document.getElementById("theme-toggle").innerHTML = "Light Mode";
+} else {
+	document.documentElement.setAttribute('data-theme', 'light');
+	window.localStorage.setItem('theme', 'light');
+	//document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+}
+
+const themeSwitcher = document.getElementById('theme-switcher');
+themeSwitcher.addEventListener('click', modeSwitcher);
+
+const themeSwitcherDesktop = document.getElementById('theme-switcher-desktop');
+themeSwitcherDesktop.addEventListener('click', modeSwitcher);
+
+function modeSwitcher() {
+	let currentMode = document.documentElement.getAttribute('data-theme');
+	if (currentMode === "dark") {
+		document.documentElement.setAttribute('data-theme', 'light');
+		window.localStorage.setItem('theme', 'light');
+		//document.getElementById("theme-toggle").innerHTML = "Dark Mode";
+	} else {
+		document.documentElement.setAttribute('data-theme', 'dark');
+		window.localStorage.setItem('theme', 'dark');
+		//document.getElementById("theme-toggle").innerHTML = "Light Mode";
+	}
+}
+
 const navMenu = document.getElementById('nav-menu'),
   toggleMenu = document.getElementById('nav-toggle'),
   closeMenu = document.getElementById('nav-close')
